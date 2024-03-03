538e8a99749f681387f509ab0b10c9429eb8dc6e
ludoguenet <ludovicguenet@gmx.com>
2024-03-03 14:39:21 +0100

    fix

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,7 +22,7 @@
                             </svg>
                         </button>
                         @else
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3 text-green-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 text-green-300">
                             <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
                         </svg>
                         @endauth
@@ -40,7 +40,7 @@
                     </div>
                 </div>
 
-                <figcaption class="mt-7">
+                <figcaption class="mt-6">
                     <div class="mt-4 flex items-center justify-center space-x-3 text-base">
                         <div class="font-semibold text-gray-900">{{ $nudge->user->name }}</div>
                         <svg viewBox="0 0 2 2" width="3" height="3" aria-hidden="true" class="fill-gray-900">
