f02c8adfc0e605af56e56c9ca19e4ca69434ed49
ludoguenet <ludovicguenet@gmx.com>
2024-03-03 22:38:07 +0100

    fix

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -1,7 +1,7 @@
 <?=
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
-<rss version="2.0">
+<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
     <channel>
         <title>
             <![CDATA[ laranudge.com ]]>
