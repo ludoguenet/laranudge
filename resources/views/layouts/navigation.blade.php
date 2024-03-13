9851a5143aa6e9c6f53231ff8589e215db89595f
ludoguenet <ludovicguenet@gmx.com>
2024-03-13 09:33:42 +0100

    fix: cursor style hover

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
                 </a>
             @endauth
 
-            <button @click="open = ! open" type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+            <button @click="open = ! open" type="button" class="hover:bg-gray-100 p-1 rounded-full inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                 </svg>
